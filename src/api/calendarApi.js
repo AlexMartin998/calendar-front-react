3df5f508d6ce53e616b342d5300f34ac597da496
Alex Martin <alextravc.342@gmail.com>
2022-09-20 11:48:21 -0500

    [ADD]: getEnvVariables  updated

--- src/api/calendarApi.js
+++ src/api/calendarApi.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 import { getEnvVariables } from '../helpers';
 
-// const { VITE_API_URL } = getEnvVariables();
+const { VITE_API_URL } = getEnvVariables();
 
 export const calendarApi = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: VITE_API_URL,
 });
 
 // Interceptores - A TODAS las peticiones: q van al back o q regresan
--- src/helpers/getEnvVariables.js
+++ src/helpers/getEnvVariables.js
@@ -1,7 +1,9 @@
 export const getEnvVariables = () => {
+  // // Al hacer el build marca error: vite issuses
   // import.meta.env;
 
   return {
-    ...import.meta.env,
+    // ...import.meta.env,
+    VITE_API_URL: import.meta.env.VITE_API_URL,
   };
 };
